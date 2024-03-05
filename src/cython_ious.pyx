a541d49bd5b15a993f4bde3cd0cdfac85ac35226
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2024-03-06 00:01:37 +0800

    Update cython_ious.pyx
    giou need consider ih<0 or iw <0

--- src/cython_ious.pyx
+++ src/cython_ious.pyx
@@ -47,58 +47,64 @@ def bbox_iou(
                     overlaps[n, k] = iw * ih / ua
     return overlaps
 
-def bbox_giou(
-        np.ndarray[DTYPE_t, ndim=2] boxes1,
-        np.ndarray[DTYPE_t, ndim=2] boxes2):
+
+def cy_bbox_overlaps_giou(
+    np.ndarray[np.float_t, ndim=2] boxes,
+    np.ndarray[np.float_t, ndim=2] query_boxes
+    ):
     """
-    Parameters
-    ----------
-    boxes1: (N, 4) ndarray of float
-    boxes2: (K, 4) ndarray of float
+    Args:
+        boxes (np.ndarray): (N, 4) ndarray of float, ltrb format
+        query_boxes (np.ndarray): (K, 4) ndarray of float, ltrb format
+
     Returns
     -------
-    overlaps: (N, K) ndarray of overlap between boxes1 and boxes2
-    """
-    cdef unsigned int N = boxes1.shape[0]
-    cdef unsigned int K = boxes2.shape[0]
-    cdef np.ndarray[DTYPE_t, ndim=2] overlaps = np.zeros((N, K), dtype=DTYPE)
-    cdef DTYPE_t iw, ih, box_area, area
-    cdef DTYPE_t ua
-    cdef unsigned int k, n
-    cdef np.ndarray[DTYPE_t, ndim=2] lt, rb, wh
+    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
 
+
+    """
+    cdef unsigned int N = boxes.shape[0] #type: ignore
+    cdef unsigned int K = query_boxes.shape[0] #type: ignore
+    cdef np.ndarray[cnp.float_t, ndim=2] overlaps = np.zeros((N, K), dtype=float) #type: ignore
+    cdef np.float_t iw, ih, box_area
+    cdef np.float_t ua
+    cdef unsigned int k, n
     for k in range(K):
         box_area = (
-                (boxes2[k, 2] - boxes2[k, 0] + 1) *
-                (boxes2[k, 3] - boxes2[k, 1] + 1)
+            (query_boxes[k, 2] - query_boxes[k, 0]) *
+            (query_boxes[k, 3] - query_boxes[k, 1])
         )
         for n in range(N):
+            box_area_ = (
+                (boxes[n, 2] - boxes[n, 0]) *
+                (boxes[n, 3] - boxes[n, 1])
+            )
+
+            lt = np.minimum(boxes[n, :2], query_boxes[k, :2])
+            rb = np.maximum(boxes[n, 2:], query_boxes[k, 2:])
+            wh = np.maximum(rb - lt, np.zeros_like(rb - lt))
+            C_area = wh[0] * wh[1]
+
             iw = (
-                    min(boxes1[n, 2], boxes2[k, 2]) -
-                    max(boxes1[n, 0], boxes2[k, 0]) + 1
+                min(boxes[n, 2], query_boxes[k, 2]) -
+                max(boxes[n, 0], query_boxes[k, 0])
             )
             if iw > 0:
                 ih = (
-                        min(boxes1[n, 3], boxes2[k, 3]) -
-                        max(boxes1[n, 1], boxes2[k, 1]) + 1
+                    min(boxes[n, 3], query_boxes[k, 3]) -
+                    max(boxes[n, 1], query_boxes[k, 1])
                 )
-
                 if ih > 0:
-                    ua = float(
-                            (boxes1[n, 2] - boxes1[n, 0] + 1) *
-                            (boxes1[n, 3] - boxes1[n, 1] + 1) +
-                            box_area - iw * ih
-                    )
+                    ua = float(box_area_ + box_area - iw * ih)
+                    overlaps[n, k] = iw * ih / ua - (C_area - ua) / C_area
 
-                    lt = np.minimum(boxes1[n, :2], boxes2[k, :2])
-                    rb = np.maximum(boxes1[n, 2:], boxes2[k, 2:])
-                    wh = np.maximum(rb - lt, np.zeros_like(rb - lt))
-                    area = wh[0] * wh[1]
-
-                    overlaps[n, k] = iw * ih / ua
-                    overlaps[n, k] = (overlaps[n, k] - (area - ua) / area)
+                else:
+                    overlaps[n, k] = - (C_area - float(box_area_ + box_area)) / C_area
+            else:
+                overlaps[n, k] = - (C_area - float(box_area_ + box_area)) / C_area
 
     return overlaps
+
 
 
 def bbox_diou(
